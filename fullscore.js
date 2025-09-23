a39e56523bc330510cd2511ba959197455f2679b
aidgn-com <info@aidgn.com>
2025-09-23 22:20:46 +0900

    Update fullscore.js

--- fullscore.js
+++ fullscore.js
@@ -226,6 +226,7 @@ class Rhythm {
 					if (ses && ses[0] === '0') return; // Return if echo=0 session exists
 				}
 				this.batch(true); // Batch confirmed
+				document.cookie = 'score=; Max-Age=0; Path=/'; // Remove score before batch
 			}, 1);
 		};
 		RHYTHM.ADD.POW && document.addEventListener('visibilitychange', () => document.visibilityState === 'hidden' && end(), { capture: true });
@@ -263,7 +264,6 @@ class Rhythm {
 			}
 			if (RHYTHM.ADD.REC === true) return; // Keep crashed sessions for recovery after abnormal exit (default: false)
 		}
-		document.cookie = 'score=; Max-Age=0; Path=/'; // Remove score before batch
 		const payload = []; // Gather echo data
 		for (let i = 1; i <= RHYTHM.MAX; i++) {
 			const ses = this.get('rhythm_' + i);
@@ -396,3 +396,4 @@ class Rhythm {
 }
 
 document.addEventListener('DOMContentLoaded', () => new Rhythm()); // Cue the performance
+
